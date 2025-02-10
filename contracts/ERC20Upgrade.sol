97b2a0a7d0fda7d3238315868eafc99b12f1c75d
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2025-02-10 10:32:12 +0100

    z

--- contracts/ERC20Upgrade.sol
+++ contracts/ERC20Upgrade.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import './lib/Events.sol';
-import './lib/Errors.sol';
+// import './lib/Events.sol';
+// import './lib/Errors.sol';
 
 contract KWAG {
     string public name;
